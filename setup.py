02b4e9fb64273a1e4af493fbfc21af646c98bdab
Yuri D. Lenskiy <yuri.lenskiy@gmail.com>
2010-12-12 21:21:06 -0500

    Added setup file.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name = 'rpass',
+        version = '7.3',
+        description = 'GPG-based commandline password manager.',
+        author = 'Yuri D. Lenskiy',
+        author_email = 'yuri.lenskiy@gmail.com',
+        py_modules = ['rpass']
+        )
