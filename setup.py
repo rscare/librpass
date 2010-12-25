e9195ac36f8ba4d64afeac8fef98ae02892347ff
Yuri D. Lenskiy <yuri.lenskiy@gmail.com>
2010-12-24 21:54:11 -0500

    Fixed setup.py to include manpage commands.

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from distutils.core import setup
+import os
+
+if os.name == 'posix':
+    from subprocess import call
+    print("Building manpage...")
+    try:
+        call('a2x -d manpage -f manpage README.asciidoc'.split(' '))
+    except OSError:
+        print("Failed to build manpage...using cached version")
 
 setup(name = 'rpass',
         version = '7.3',
@@ -6,5 +15,6 @@ setup(name = 'rpass',
         author = 'Yuri D. Lenskiy',
         author_email = 'yuri.lenskiy@gmail.com',
         py_modules = ['rpass'],
-        scripts = ['rpass']
+        scripts = ['rpass'],
+        data_files = [('share/man/man1', ['rpass.1'])]
         )
